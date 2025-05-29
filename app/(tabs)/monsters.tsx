028f9e5a87d749159cc356c42472f871d4bffe69
Luiz Henrique E. N. Santos <luizhenrique.egydio@gmailcom>
2025-05-29 16:21:03 -0300

    completed monster tab

--- app/(tabs)/monsters.tsx
+++ app/(tabs)/monsters.tsx
@@ -7,7 +7,6 @@ import {
   ActivityIndicator,
   FlatList,
   Image,
-  ImageBackground,
   Modal,
   Pressable,
   SafeAreaView,
@@ -16,6 +15,7 @@ import {
   TouchableOpacity,
   View
 } from "react-native";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function Monsters() {
   const [selected, setSelected] = useState<string>("");
@@ -27,18 +27,19 @@ export default function Monsters() {
   useFocusEffect(
     useCallback(() => {
       const fecthData = async () => {
+        setIsLoading(true)
         const monsterAPI = await getAllMonstersName();
         setMonsters(monsterAPI);
         setSelected(monsters[3].name)
       };
 
       fecthData();
+      setIsLoading(false)
     }, [])
   );
 
   useEffect(() => {
     const preftchData = async () => {
-
       setMonsterInfo(await getMonsterInfo(returnURL(selected)));
     };
 
@@ -49,125 +50,140 @@ export default function Monsters() {
     }, 1500);
   }, [selected]);
   return (
-    <SafeAreaView
-      style={{ flex: 1, justifyContent: "flex-start", paddingTop: 50 }}
-    >
-      <Modal visible={visivel} transparent={false}>
-        <Pressable onPress={() => setVisivel(false)}>
-          <ImageBackground style={styles.image} source={{ uri: monsterInfo?.image }} />
-        </Pressable>
-      </Modal>
-      <View
-        style={{
-          flexDirection: "row",
-          margin: 12,
-          borderBottomColor: "black",
-          borderBottomWidth: 0.5,
-          paddingBottom: 10,
-        }}
+
+    <SafeAreaProvider>
+      <SafeAreaView
+        style={{ alignItems: 'stretch' }}
       >
-        <Pressable
-          onPress={() => {
-            setVisivel(true);
-          }}
-        >
-          <Image
-            style={{ width: 150, height: 150 }}
-            source={{
-              uri: monsterInfo?.image,
-            }}
-            resizeMode="stretch"
-          />
-        </Pressable>
         <View
           style={{
-            flex: 1,
-            alignItems: "center",
-            justifyContent: "space-between",
+            flexDirection: "row",
+            margin: 12,
+            borderBottomColor: "black",
+            borderBottomWidth: 0.5,
+            paddingBottom: 10,
           }}
         >
-          <Text
-            style={{
-              fontSize: 26,
-              fontWeight: 900,
-              flexShrink: 1,
-              textAlign: "center",
-              textAlignVertical: "center",
-              paddingTop: 5,
+          <Pressable
+            onPress={() => {
+              setVisivel(true);
             }}
           >
-            {monsterInfo?.name}
-          </Text>
-          {/* // #region Colunas dos Status */}
+            <Image
+              style={{ width: 150, height: 150 }}
+              source={{
+                uri: monsterInfo?.image,
+              }}
+              resizeMode="stretch"
+            />
+          </Pressable>
           <View
             style={{
-              flexDirection: "row",
-              marginStart: 12,
-              gap: 1,
+              flex: 1,
+              alignItems: "center",
+              justifyContent: "space-between",
             }}
           >
-            <View
+            <Text
               style={{
-                paddingEnd: 4,
-                borderEndWidth: 0.5,
-                borderEndColor: "black",
+                fontSize: 26,
+                fontWeight: 900,
+                flexShrink: 1,
+                textAlign: "center",
+                textAlignVertical: "center",
+                paddingTop: 5,
               }}
             >
-              <MonsterStatusText
-                statusName="Força"
-                statusValue={monsterInfo?.strength}
-              />
-              <MonsterStatusText
-                statusName="Destreza"
-                statusValue={monsterInfo?.dexterity}
-              />
-              <MonsterStatusText
-                statusName="Constituição"
-                statusValue={monsterInfo?.constitution}
-              />
+              {monsterInfo?.name}
+            </Text>
+            {/* // #region Colunas dos Status */}
+            <View
+              style={{
+                flexDirection: "row",
+                marginStart: 12,
+                gap: 1,
+              }}
+            >
+              <View
+                style={{
+                  paddingEnd: 4,
+                  borderEndWidth: 0.5,
+                  borderEndColor: "black",
+                }}
+              >
+                <MonsterStatusText
+                  statusName="Força"
+                  statusValue={monsterInfo?.strength}
+                />
+                <MonsterStatusText
+                  statusName="Destreza"
+                  statusValue={monsterInfo?.dexterity}
+                />
+                <MonsterStatusText
+                  statusName="Constituição"
+                  statusValue={monsterInfo?.constitution}
+                />
+              </View>
+              <View style={{ paddingStart: 3, paddingEnd: 3 }}>
+                <MonsterStatusText
+                  statusName="Inteligência"
+                  statusValue={monsterInfo?.intelligence}
+                />
+                <MonsterStatusText
+                  statusName="Sabedoria"
+                  statusValue={monsterInfo?.wisdom}
+                />
+                <MonsterStatusText
+                  statusName="Carisma"
+                  statusValue={monsterInfo?.charisma}
+                />
+              </View>
+              {/* // #endregion */}
             </View>
-            <View style={{ paddingStart: 3, paddingEnd: 3 }}>
-              <MonsterStatusText
-                statusName="Inteligência"
-                statusValue={monsterInfo?.intelligence}
-              />
-              <MonsterStatusText
-                statusName="Sabedoria"
-                statusValue={monsterInfo?.wisdom}
-              />
-              <MonsterStatusText
-                statusName="Carisma"
-                statusValue={monsterInfo?.charisma}
-              />
-            </View>
-            {/* // #endregion */}
           </View>
         </View>
-      </View>
-      {isLoading ?
-        <Modal style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}
-          animationType="fade"
-          visible={isLoading} >
-          <ActivityIndicator size={'large'} />
-        </Modal> :
-        <FlatList data={monsters.map((m) => m.name)}
-          renderItem={(item) => (
-            <TouchableOpacity onPress={() => {
+
+        <FlatList
+          data={monsters.map((m) => m.name)}
+          keyExtractor={(dados) => dados}
+          windowSize={2}
+          renderItem={(item) => (item.index % 2 === 0 ?
+            <TouchableOpacity style={styles.monsterList} onPress={() => {
               setIsLoading(true)
               setSelected(item.item)
-            }}>
-              <Text>{item.item}</Text>
+            }}
+            >
+              <Text style={styles.fontMonsterList}>{item.item}</Text>
+            </TouchableOpacity> : <TouchableOpacity style={[styles.monsterList, styles.monsterListOdd]} onPress={() => {
+              setIsLoading(true)
+              setSelected(item.item)
+            }}
+            >
+              <Text style={styles.fontMonsterList}>{item.item}</Text>
             </TouchableOpacity>
           )} />
-      }
-      {/* 
-      <SelectList
-        data={monsters.map((monster) => monster.name)}
-        save="value"
-        search={false}
-        setSelected={(v: string) => setSelected(v)}
-      /> */}
-    </SafeAreaView>
+        <Modal visible={visivel} transparent={true} >
+          <View style={{
+            padding: 20, backgroundColor: 'white'
+          }}>
+            < Pressable onPress={() => setVisivel(false)}>
+              <Image style={styles.image} resizeMode='center' source={{ uri: monsterInfo?.image }} />
+            </Pressable>
+          </View>
+        </Modal>
+        {
+          isLoading ?
+
+            <Modal
+              animationType="fade"
+              visible={isLoading} >
+              <ActivityIndicator size="large" />
+            </Modal>
+            : null
+        }
+      </SafeAreaView >
+
+    </SafeAreaProvider >
   );
 
 
@@ -181,5 +197,19 @@ const styles = StyleSheet.create({
   image: {
     width: '100%',
     height: '100%',
+  },
+  monsterList: {
+    borderWidth: 1,
+    margin: 0.5,
+    backgroundColor: 'purple',
+  },
+  fontMonsterList: {
+    fontSize: 22,
+    textAlign: 'center',
+    fontWeight: 600,
+    color: 'white'
+  },
+  monsterListOdd: {
+    backgroundColor: 'darkslateblue'
   }
 })
